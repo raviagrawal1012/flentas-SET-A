aeebc03badbe44caecb658e321c911437c162028
Ravi Agrawal <51258970+raviagrawal1012@users.noreply.github.com>
2020-07-08 12:45:03 +0530

    SET A

---
+++ sherlock.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+bool checkPermutation(string s1, string s2)
+{
+    unordered_map<char, int> exist;
+    for(int i=0; i<s1.size();i++)
+    {
+        if(exist.count(s1[i]) > 0)
+        {
+            exist[s1[i]]++;
+        }
+        else
+        {
+            exist[s1[i]] = 1;
+        }
+    }
+
+    for(int i=0; i<s2.size();i++)
+    {
+        if(exist.count(s2[i]) == 0)
+        {
+            return false;
+        }
+        else
+        {
+            exist[s2[i]]--;
+        }
+    }
+
+    return true;
+
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    vector<string> output;
+    while(t > 0)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+        bool ans = checkPermutation(s1, s2);
+        if(ans)
+        {
+            output.push_back("YES");
+        }
+        else{
+            output.push_back("NO");
+        }
+
+        t--;
+    }
+
+    for(int i=0; i<output.size();i++)
+    {
+        cout << output[i] << endl;
+    }
+}
